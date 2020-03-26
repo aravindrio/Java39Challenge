b8d642465a93115dece2137dad8d1e133593db01
Aravind R <aravind.gtr@gmail.com>
2020-03-26 01:12:36 -0700

    Update example.java

--- src/example.java
+++ src/example.java
@@ -3,9 +3,9 @@ public class example {
     public static void main(String args[]){
       double var1 = 20.00d;
       double var2 = 93.00d;
-      double var3 = (var1 + var2) * 100.00;
+      double var3 = (var1 + var2) * 100.00d;
 
-      double rem = var3 % 40.00;
+      double rem = var3 % 40.00d;
 
       boolean output = (rem == 0) ? true : false;
       System.out.println(output);
