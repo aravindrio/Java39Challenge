218a1be08045c14b9ea5f61fd26f80074aadd76f
aravindrio <aravind.gtr@gmail.com>
2020-03-26 00:53:38 -0700

    Initial commit

---
+++ src/example.java
@@ -0,0 +1,18 @@
+public class example {
+
+    public static void main(String args[]){
+      double var1 = 20.00;
+      double var2 = 93.00;
+      double var3 = (var1 + var2) * 100.00;
+
+      double rem = var3 % 40.00;
+
+      boolean output = (rem == 0) ? true : false;
+      System.out.println(output);
+
+      if(output != true){
+          System.out.println("Got some remainder");
+      }
+
+    }
+}
